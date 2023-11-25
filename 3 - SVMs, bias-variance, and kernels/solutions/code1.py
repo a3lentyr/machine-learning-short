1af139ce4524952210a37bfc16e7892fe168ea35
Jonathan Sprauel <jonathan.sprauel@thalesaleniaspace.com>
2023-11-25 21:55:27 +0100

    removed all but 2 notebooks

--- 3 - SVMs, bias-variance, and kernels/solutions/code1.py
+++
@@ -1,9 +0,0 @@
-### WRITE YOUR CODE HERE
-# If you get stuck, uncomment the line above to load a correction in this cell (then you can execute this code).
-
-w = np.array([0.7, -1])
-w_norm = np.linalg.norm(w)
-w = w/w_norm
-w0 = 0.4/w_norm
-M = y*(np.dot(X,w) + w0)
-print("margin =", np.min(M))
